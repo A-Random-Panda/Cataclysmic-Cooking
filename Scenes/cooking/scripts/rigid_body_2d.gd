9e3bb74aab674a6240f943a35ffb5faea8cbecda
justanothernormalusername <marcusdeng2019@gmail.com>
2026-08-26 20:37:41 -0700

    less jank item physics

--- Scenes/cooking/scripts/rigid_body_2d.gd
+++ Scenes/cooking/scripts/rigid_body_2d.gd
@@ -1,9 +1,12 @@
 extends RigidBody2D
 
+const X_THROW_STRENGTH: float = 0.8
+const Y_THROW_STRENGTH: float = 0.4
+
 var mouse_on: bool = false
 var dragging: bool = false
 var drag_offset: Vector2 = Vector2.ZERO
-var last_mouse_velocities: PackedVector2Array = [Vector2.ZERO];
+var last_mouse_position: Vector2 = Vector2.ZERO
 
 
 # Called when the node enters the scene tree for the first time.
@@ -12,42 +15,23 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(_delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	if !dragging and mouse_on and Input.is_action_pressed("left_click"):
 		drag_offset = global_position - get_global_mouse_position()
 		dragging = true
-		
+
 	if dragging and Input.is_action_pressed("left_click"):
+		last_mouse_position = get_global_mouse_position()
 		freeze = true
 		set_global_position(get_global_mouse_position() + drag_offset)
+
 	elif dragging:
 		freeze = false
 		dragging = false
-		
-		# Weighted average of 5 vectors in last_mouse_velocities and apply linear velocity
-		var xSum: float = 0
-		var ySum: float = 0
-		var weightSum: int = 0
-		for i in range(len(last_mouse_velocities)):
-			var mouse_velocity: Vector2 = last_mouse_velocities[i]
-			var weight = i + 1
-			weightSum += weight
-			xSum += weight * mouse_velocity.x
-			ySum += weight * mouse_velocity.y
-		xSum /= weightSum
-		ySum /= weightSum
-		print(Vector2(xSum, ySum))
-		linear_velocity = Vector2(xSum, ySum)
-		
-		print(last_mouse_velocities)
-		# Remember to reset the stale data!
-		last_mouse_velocities = [Vector2.ZERO]
-
-func _input(event: InputEvent) -> void:
-	if event is InputEventMouseMotion and dragging:
-		last_mouse_velocities.append(event.velocity)
-		if len(last_mouse_velocities) > 3:
-			last_mouse_velocities.remove_at(0)
+	
+		var unscaled_velocity = (get_global_mouse_position() - last_mouse_position) / delta
+	
+		linear_velocity = Vector2(X_THROW_STRENGTH * unscaled_velocity.x, Y_THROW_STRENGTH * unscaled_velocity.y)
 
 func _mouse_enter() -> void:
 	mouse_on = true
