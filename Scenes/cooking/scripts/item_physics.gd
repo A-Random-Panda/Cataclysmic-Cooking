5a589f1bf8c2722a3453e68c530f806688f3dcd2
justanothernormalusername <marcusdeng2019@gmail.com>
2026-08-27 02:48:01 -0700

    carrot bounciness and clamping

--- Scenes/cooking/scripts/item_physics.gd
+++ Scenes/cooking/scripts/item_physics.gd
@@ -1,17 +1,24 @@
 extends RigidBody2D
 
-const X_THROW_STRENGTH: float = 0.8
-const Y_THROW_STRENGTH: float = 0.4
+const X_THROW_STRENGTH: float = 1
+const Y_THROW_STRENGTH: float = 1
+
+const CLAMP_CIRCLE_SHAVE: float = 0.8
 
 var mouse_on: bool = false
 var dragging: bool = false
 var drag_offset: Vector2 = Vector2.ZERO
 var last_mouse_position: Vector2 = Vector2.ZERO
 
+var clamp_vector: Vector2 = Vector2.ZERO
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	# Calculate the clamping radius
+	var clamp_radius: float = 0
+	for vector: Vector2 in $Hitbox.polygon:
+		clamp_radius = max(clamp_radius, vector.distance_to(Vector2.ZERO)) * CLAMP_CIRCLE_SHAVE
+	clamp_vector = Vector2(clamp_radius, clamp_radius)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -23,7 +30,11 @@ func _physics_process(delta: float) -> void:
 	if dragging and Input.is_action_pressed("left_click"):
 		last_mouse_position = get_global_mouse_position()
 		freeze = true
-		set_global_position(get_global_mouse_position() + drag_offset)
+		
+		# Position clamping
+		var target_position: Vector2 = get_global_mouse_position() + drag_offset
+		target_position = target_position.clamp(Vector2.ZERO + clamp_vector, get_viewport_rect().size - clamp_vector)
+		set_global_position(target_position)
 
 	elif dragging:
 		freeze = false
