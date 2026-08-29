fadddec34547281cf06676fa2297e3fdbb9d8dd8
justanothernormalusername <marcusdeng2019@gmail.com>
2026-08-29 04:14:32 -0700

    Refactored item to inventory signals for modularity

--- Scenes/cooking/scripts/inventory_box.gd
+++ Scenes/cooking/scripts/inventory_box.gd
@@ -1,3 +1,4 @@
+class_name InventoryArea
 extends Area2D
 
 # Called when the node enters the scene tree for the first time.
@@ -7,36 +8,24 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
-"""
-func _on_body_entered(body: Node2D):
-	if body is Item:
-		body.in_inventory = true
-		
-		if !body.hidden_from_inventory:
-			GlobalUI.inventory.add_node(body)
-			body.queue_free()
 
-func _on_body_exited(body: Node2D) -> void:
-	if body is Item:
-		body.in_inventory = false
-"""
+static func add_to_inventory(node: Node2D) -> void:
+	GlobalUI.inventory.add_node(node)
+	node.queue_free()
+
+
+func _on_area_entered(area: Area2D) -> void:
+	var node: Node2D = area.get_parent()
+	if node is Item:
+		node.in_inventory = true
+
+func _on_area_exited(area: Area2D) -> void:
+	var node: Node2D = area.get_parent()
+	if node is Item:
+		node.in_inventory = false
+
+
 func _on_button_pressed() -> void:
 	var node: Item = GlobalUI.inventory.inventory_items[0].spawn(get_global_mouse_position())
 	add_sibling(node)
 	# node.hidden_from_inventory = true
-
-
-func _on_area_entered(area: Area2D) -> void:
-	var body: Node2D = area.get_parent()
-	if body is Item:
-		body.in_inventory = true
-		
-		if !body.hidden_from_inventory:
-			GlobalUI.inventory.add_node(body)
-			body.queue_free()
-
-
-func _on_area_exited(area: Area2D) -> void:
-	var body: Node2D = area.get_parent()
-	if body is Item:
-		body.in_inventory = false
--- core/scripts/item_physics.gd
+++ core/scripts/item_physics.gd
@@ -8,9 +8,6 @@ extends RigidBody2D
 
 @export var CLAMP_CIRCLE_SHAVE: float = 0.8
 
-@onready var INVENTORY_AREA_2D := get_parent().get_node("InventoryBox")
-
-var hidden_from_inventory := true
 var in_inventory := false
 
 var mouse_on := false
@@ -31,10 +28,10 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
+	print(in_inventory)
 	if !dragging and mouse_on and Input.is_action_pressed("left_click"):
 		drag_offset = global_position - get_global_mouse_position()
 		dragging = true
-		hidden_from_inventory = false
 
 	if dragging and Input.is_action_pressed("left_click"):
 		last_mouse_position = get_global_mouse_position()
@@ -49,19 +46,12 @@ func _physics_process(delta: float) -> void:
 		freeze = false
 		dragging = false
 		
-		# if in_inventory:
-		# 	INVENTORY_AREA_2D._on_body_entered(self)
-		
-		# allow_inventory_detection()
+		if in_inventory:
+			InventoryArea.add_to_inventory(self)
 		
 		# Apply velocity
 		var unscaled_velocity = (get_global_mouse_position() - last_mouse_position) / delta
 		linear_velocity = Vector2(X_THROW_STRENGTH * unscaled_velocity.x, Y_THROW_STRENGTH * unscaled_velocity.y)
-
-# Awaits for a frame after deselect before hiding from inventory
-func allow_inventory_detection() -> void:
-	await get_tree().physics_frame
-	hidden_from_inventory = true
 
 func _on_select_box_mouse_entered() -> void:
 	mouse_on = true
