979a79a2a783e7ea24451758d6829fc7abaac1e9
A-Random-Panda <yaoh667@gmail.com>
2026-08-30 01:18:32 -0700

    worker num + finished entrance

--- Scenes/entrance/hyvariables.gd
+++ Scenes/entrance/hyvariables.gd
@@ -4,7 +4,11 @@ var vistor_time: float = 0.0
 var vistor_here: bool = false
 var fire_list: Array[Vector2] = []
 var timer: float = 0.0
-var fire_sabo: bool = false
+var fire_sabo: bool = true
+var in_sabo: bool = false
+var timer2: float = 0.0
+var insta_fire: int = 0
+var workers: int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -14,6 +18,15 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if fire_sabo:
-		Hyvariables.timer += delta
+		timer += delta
+	if not in_sabo:
+		timer2 += delta
+		if timer2 > 8:
+			insta_fire += 1
+			timer2 = 0
+			
+			
+		
+	
 	if not vistor_here:
 		Hyvariables.vistor_time += delta
